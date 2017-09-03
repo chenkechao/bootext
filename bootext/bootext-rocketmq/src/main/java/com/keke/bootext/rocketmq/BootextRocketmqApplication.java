3c66c1479e71d16d48c42b96c97a909ac6d4ed77
chenkechao <chenkechao123@163.com>
2017-09-03 14:03:36 +0800

    spring-dubbo

--- bootext/bootext-rocketmq/src/main/java/com/keke/bootext/rocketmq/BootextRocketmqApplication.java
+++ bootext/bootext-rocketmq/src/main/java/com/keke/bootext/rocketmq/BootextRocketmqApplication.java
@@ -2,10 +2,12 @@ package com.keke.bootext.rocketmq;
 
 import com.keke.bootext.rocketmq.annotation.EnableMQConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableMQConfiguration
+@EnableAutoConfiguration
 public class BootextRocketmqApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootextRocketmqApplication.class, args);
