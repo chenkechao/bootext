8be75fcadf7ee74b1163bc40b7be6cd12dac696c
chenkechao <chenkechao123@163.com>
2017-09-03 08:10:41 +0800

    spring-dubbo

--- bootext/bootext-dubbo/src/main/java/com/keke/bootext/dubbo/BootextDubboApplication.java
+++ bootext/bootext-dubbo/src/main/java/com/keke/bootext/dubbo/BootextDubboApplication.java
@@ -3,6 +3,9 @@ package com.keke.bootext.dubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://github.com/Xiaobaxi/spring-boot-starter-dubbo.git
+ */
 @SpringBootApplication
 public class BootextDubboApplication {
     public static void main(String[] args) {
